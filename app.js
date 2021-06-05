f951e2d130f50f933e0216070bd5a94f2746d507
Jaquespd <jaquespd@gmail.com>
2021-06-05 11:30:30 -0300

    Test2

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send('Muitxooo louuuucoooooo!')
+})
+
+app.get('/adonias', (req, res) => {
+  res.send({
+    id: 1,
+    nome: "S01",
+    descrição: "Sensor Temperatura",
+    dados: [
+      {
+        data: "01/06/2021 12:00",
+        valor: "12 graus"
+      },
+      {
+        data: "02/06/2021 12:00",
+        valor: "8 graus"
+      },
+      {
+        data: "03/06/2021 12:00",
+        valor: "20 graus"
+      },
+    ]
+  });
 })
 
 
